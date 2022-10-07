a3b6389dc437320c5c5d2759539eb28c0a27bbb8
dazzul94 <dazzul94@gmail.com>
2022-10-07 22:27:40 +0900

    method2

---
+++ source/ch06/CallStackTest.java
@@ -0,0 +1,13 @@
+package source.ch06;
+
+public class CallStackTest {
+    public static void main(String[] args) {
+        firstMethod();  // static메서드는 객체 생성없이 호출가능
+    }
+    static void firstMethod() {
+        secondMethod();
+    }
+    static void secondMethod() {
+        System.out.println("secondMethod()");
+    }
+}
