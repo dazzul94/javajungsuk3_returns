a7383f5e8746b91491f90ed69a7b06d4fd7f7f15
dazzul94 <dazzul94@gmail.com>
2022-12-21 18:48:54 +0900

    8. Exception

---
+++ source/ch08/ExceptionEx10.java
@@ -0,0 +1,7 @@
+package source.ch08;
+
+public class ExceptionEx10 {
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+}
---
+++ source/ch08/FinallyTest2.java
@@ -0,0 +1,23 @@
+package source.ch08;
+
+public class FinallyTest2 {
+    public static void main(String[] args) {
+        try {
+            startInstall();
+            copyFiles();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            deleteTempFiles();
+        }
+    }
+    static void startInstall() {
+
+    }
+    static void copyFiles() {
+
+    }
+    static void deleteTempFiles() {
+
+    }
+}
